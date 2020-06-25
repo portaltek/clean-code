e91fa664f881f138f2620105a18ebec73239e890
steek09@gmail.com <steek09@gmail.com>
2020-06-24 22:02:55 -0600

    feat: web API hello created

---
+++ src/main/java/portaltek/cleancode/api/web/CleanCodeApplication.java
@@ -0,0 +1,22 @@
+package portaltek.cleancode.api.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CleanCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanCodeApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+
+}
