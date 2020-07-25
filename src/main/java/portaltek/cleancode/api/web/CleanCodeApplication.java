2fa482e767d5b09e2973c9c83af539e72d51c174
steek09@gmail.com <steek09@gmail.com>
2020-07-25 17:17:04 -0600

    adding security to actuator endpoints. Shutdown endpoint works

--- src/main/java/portaltek/cleancode/api/web/CleanCodeApplication.java
+++ src/main/java/portaltek/cleancode/api/web/CleanCodeApplication.java
@@ -1,7 +1,11 @@
 package portaltek.cleancode.api.web;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +22,17 @@ public class CleanCodeApplication {
         return "Hello World";
     }
 
+
+}
+
+
+@Configuration
+class ApiSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().
+                requestMatcher(EndpointRequest.toAnyEndpoint()).authorizeRequests().anyRequest().permitAll();
+    }
 
 }
