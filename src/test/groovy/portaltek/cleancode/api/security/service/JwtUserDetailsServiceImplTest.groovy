057297dbb1d20dc7313b0cdf5adcf98c1c49af77
steek09@gmail.com <steek09@gmail.com>
2020-10-11 14:53:58 -0600

    feat: adding JwtUserDetailsServiceImpl unit test

--- src/test/groovy/portaltek/cleancode/api/security/service/JwtUserDetailsServiceImplTest.groovy
+++ src/test/groovy/portaltek/cleancode/api/security/service/JwtUserDetailsServiceImplTest.groovy
@@ -1,13 +1,44 @@
 package portaltek.cleancode.api.security.service
 
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import portaltek.cleancode.api.model.User
+import portaltek.cleancode.api.repository.UserRepo
 import spock.lang.Specification
 
 class JwtUserDetailsServiceImplTest extends Specification {
-    def "load existing user by username"() {
+
+    UserRepo userRepo
+    User user
+    UserDetailsService service
+
+    void setup() {
+        userRepo = Mock(UserRepo)
+        user = new User("Username","Password", true)
+        service = new JwtUserDetailsServiceImpl(userRepo);
+    }
+
+    def "load existing user by username returns UserDetails"() {
+        given:
+        userRepo.findByUsername("Username") >> user
+
         when:
-        String hello = "hello"
+        def userFound = service.loadUserByUsername("Username")
+
         then:
-        hello == "hello"
+        userFound != null
+    }
+
+    def "load non-existing user by username throws UsernameNotFoundException"() {
+        given:
+
+        when:
+        def userFound = service.loadUserByUsername("Username")
+
+        then:
+        UsernameNotFoundException ex = thrown()
+        ex.message == "No user found with username 'Username'."
 
     }
+
 }
