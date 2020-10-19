efb5e48d07f7f263742f4794c4c279a9f1a1270b
steek09@gmail.com <steek09@gmail.com>
2020-10-18 18:59:57 -0600

    feat: adding scripts and tasks to create itest db

---
+++ src/itest/resources/docker/db.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+create_db() {
+    docker run --name cleancode_db_itest --env MYSQL_ROOT_PASSWORD=root_password -p 4306:3306 -d mysql
+}
+
+delete_db() {
+    docker rm -f cleancode_db_itest
+}
+
+#update_db() {
+#    docker rm -f cleancode_db_itest
+#}
+
+case "${1}" in
+    "create") echo Creating Integration Test DB && create_db;;
+    "delete") echo Deleting Integration Test DB && delete_db;;
+esac
