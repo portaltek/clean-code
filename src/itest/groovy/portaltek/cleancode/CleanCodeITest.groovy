a647836e73d196f8197c344e3ad6c88712d5175c
steek09@gmail.com <steek09@gmail.com>
2020-10-19 23:43:36 -0600

    feat: adding simple groovy tests

---
+++ src/itest/groovy/portaltek/cleancode/CleanCodeITest.groovy
@@ -0,0 +1,33 @@
+package portaltek.cleancode
+
+
+import portaltek.cleancode.spi.datastore.model.User
+import spock.lang.Specification
+
+class SimpleITest extends Specification {
+    def "load from dummy class"() {
+        given:
+        def url = "hello";
+        def user = new User("username", "password", true)
+        when:
+        user.username = url;
+
+        then:
+        user.username == url;
+        //true == false
+    }
+}
+
+class SimpleITest2 extends Specification {
+    def "load from dummy class"() {
+        given:
+        def url = "hello";
+        def user = new User("username", "password", true)
+        when:
+        user.username = url;
+
+        then:
+        user.username == url;
+        //true == false
+    }
+}
