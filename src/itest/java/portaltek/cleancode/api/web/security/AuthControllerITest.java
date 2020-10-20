2f2b5ad334d972333900b238fcc4e424f27ce61f
steek09@gmail.com <steek09@gmail.com>
2020-10-19 21:26:31 -0600

    feat: first integration test to web api controllers

--- src/itest/java/portaltek/cleancode/api/web/security/AuthControllerITest.java
+++ src/itest/java/portaltek/cleancode/api/web/security/AuthControllerITest.java
@@ -4,46 +4,51 @@
 package portaltek.cleancode.api.web.security;
 
 
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import portaltek.cleancode.CleanCodeApp;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.assertj.core.api.BDDAssertions.then;
 
 //@RunWith(SpringRunner.class)
 //@SpringBootTest(webEnvironment = RANDOM_PORT)
-@SpringBootTest
+//@SpringBootTest
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = CleanCodeApp.class, webEnvironment = RANDOM_PORT)
 class AuthControllerITest {
 
-//    @LocalServerPort
-//    private int port;
+    @LocalServerPort
+    private int port;
 
     @Autowired
     private AuthController controller;
-//    @Autowired
-//    private TestRestTemplate restTemplate;
+    @Autowired
+    private TestRestTemplate testRestTemplate;
 
-    //String URL = "http://localhost:" + port + "/api/open/hello/";
-    String URL = "http://localhost:8080/api/open/hello/";
-    String EXPECTED_MSG = "{\"message\":\"Hello World!!!\"}";
+    final String EXPECTED_MSG = "{\"message\":\"Hello World!!!\"}";
 
 
     @Test
     public void contextLoads() {
         assertThat(controller).isNotNull();
+        final String url = "http://localhost:" + this.port + "/api/open/hello";
+        ResponseEntity<String> entity = this.testRestTemplate
+                .getForEntity(url, String.class);
+
+        then(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
-//    @Test
-//    public void greetingShouldReturnDefaultMessage() {
-//        assertThat(restTemplate.getForObject(URL, String.class))
-//                .contains(EXPECTED_MSG);
-//    }
+
 
 }
 
