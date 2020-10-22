d2bf9c1e7c1459c418c622c18914a231dc03baba
steek09@gmail.com <steek09@gmail.com>
2020-10-21 20:56:54 -0600

    chore: Renaming TokenControllerITest.java

--- src/itest/java/portaltek/cleancode/CleanCodeAppITest.java
+++ src/itest/java/portaltek/cleancode/CleanCodeAppITest.java
@@ -5,13 +5,16 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
+import portaltek.cleancode.itest.CleanCodeAppITestConfig;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = CleanCodeApp.class, webEnvironment = RANDOM_PORT)
+@Import(CleanCodeAppITestConfig.class)
 class CleanCodeAppITest {
 
     @Autowired
--- src/itest/java/portaltek/cleancode/api/web/token/TokenControllerITest.java
+++ src/itest/java/portaltek/cleancode/api/web/token/TokenControllerITest.java
@@ -1,4 +1,4 @@
-package portaltek.cleancode.api.web.user;
+package portaltek.cleancode.api.web.token;
 
 
 import org.junit.jupiter.api.BeforeEach;
@@ -7,7 +7,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.junit4.SpringRunner;
 import portaltek.cleancode.CleanCodeApp;
@@ -16,40 +15,48 @@ import portaltek.cleancode.itest.CleanCodeAppITestConfig;
 
 import static org.assertj.core.api.BDDAssertions.then;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpStatus.OK;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = CleanCodeApp.class, webEnvironment = RANDOM_PORT)
 @Import(CleanCodeAppITestConfig.class)
-class UserControllerITest {
+class TokenControllerITest {
+
+    final String EXPECTED_MSG = "{\"message\":\"Pong!\"}";
 
     @Autowired
     Api api;
     HttpHeaders headers;
     String pingUrl;
+    String createUrl;
 
     @BeforeEach
     public void init() {
         headers = api.header("admin", "admin");
-        pingUrl = api.url("/api/auth/user/ping");
+        pingUrl = api.url("/api/open/token/ping");
+        createUrl = api.url("/api/open/token/create");
+    }
+
+    @Test
+    public void postValidLogin_shouldReturnToken() {
+
+        String token = api.token("admin", "admin");
+        then(token).isNotNull();
     }
 
     @Test
     public void getPing_shouldReturnPong() {
 
-        var entity = new HttpEntity<>("ping", headers);
         var response = api.rest()
-                .exchange(pingUrl, GET, entity, String.class);
+                .getForEntity(pingUrl, String.class);
 
         then(response.getStatusCode()).isEqualTo(OK);
-        then(response.getBody()).isEqualTo("Pong!");
+        then(response.getBody()).isEqualTo(EXPECTED_MSG);
     }
 
 
 }
-
 
 
 
