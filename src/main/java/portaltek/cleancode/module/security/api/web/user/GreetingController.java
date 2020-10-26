b0d12847564af10478858edce3d6f2d26fa4552b
steek09@gmail.com <steek09@gmail.com>
2020-10-25 22:49:29 -0600

    feat: adding profile

--- src/main/java/portaltek/cleancode/module/security/api/web/user/GreetingController.java
+++ src/main/java/portaltek/cleancode/module/security/api/web/user/GreetingController.java
@@ -3,27 +3,25 @@ package portaltek.cleancode.module.security.api.web.user;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import portaltek.cleancode.infra.web.ServerResponse;
 
 
 @RestController
-@RequestMapping("/api/auth/")
 class GreetingController {
 
 
-    @RequestMapping(value = "admin", method = RequestMethod.GET)
+    @GetMapping(value = "/api/auth/admin")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public ResponseEntity<ServerResponse> greetingAdmin() throws Exception {
+    public ResponseEntity<ServerResponse> greetingAdmin() {
 
         String msg = "Hello Authenticated Admin";
         return ResponseEntity.ok(new ServerResponse(msg));
     }
 
-    @RequestMapping(value = "user", method = RequestMethod.GET)
-    public ResponseEntity<ServerResponse> greetingUser() throws Exception {
+    @GetMapping(value = "/api/auth/user")
+    public ResponseEntity<ServerResponse> greetingUser() {
 
         String msg = "Hello Authenticated User";
         return ResponseEntity.ok(new ServerResponse(msg));
