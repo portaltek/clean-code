4658ae4755a7d6ec140d8972302df9424f0e626b
steek09@gmail.com <steek09@gmail.com>
2020-10-25 19:27:23 -0600

    feat: adding UserRepoInMemoryTest.groovy

--- src/main/java/portaltek/cleancode/module/security/spi/repo/UserRepo.java
+++ src/main/java/portaltek/cleancode/module/security/spi/repo/UserRepo.java
@@ -1,11 +1,7 @@
-/**
- *
- */
 package portaltek.cleancode.module.security.spi.repo;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import portaltek.cleancode.module.security.spi.repo.User;
 
 
 interface UserRepo extends JpaRepository<User, Long> {
---
+++ src/test/groovy/portaltek/cleancode/module/security/spi/repo/UserRepoInMemoryTest.groovy
@@ -0,0 +1,14 @@
+package portaltek.cleancode.module.security.spi.repo
+
+
+import spock.lang.Specification
+
+class UserRepoInMemoryTest extends Specification {
+
+//    def repo = new UserRepoInMemory();
+
+    def "save user"() {
+
+
+    }
+}
